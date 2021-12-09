4a1f2553b060d5914448c8f7aeb30604c18d807c
samhesselmans <samcasperhesselmans@gmail.com>
2021-12-09 09:17:49 +0100

    Working

--- efficient_route_solver.py
+++ efficient_route_solver.py
@@ -1,9 +1,10 @@
 import numpy as np
+import time
 from numpy.core.numeric import Infinity
 from numpy.lib.financial import _fv_dispatcher;
 from scipy.optimize import fsolve
 
-power_settings = [50,100,150,200,250]
+power_settings = [0,50,100,150,200,250]
 cyclist_power = 150
 
 Cd = 1.18
@@ -12,7 +13,7 @@ Ro = 1.18
 Cr = 0.01
 g= 9.81
 
-bike_mass = 150
+bike_mass = 250
 
 def calcFD(v):
     return Cd * A * Ro * pow(v,2) /2
@@ -33,19 +34,19 @@ for power in power_settings:
 #Electric power for the different bike settings
 
 def generateCustomers(num_customers, max_speed):
-    customer_distances = np.random.randint(1000,30000,num_customers)
+    customer_distances = np.random.randint(200,2000,num_customers)
     customer_timewindows=[]
     #Time in minutes
     prev_min = 0
 
     max_time_window_start_variation = 30
-    min_window_size = 30
-    max_window_size = 180
+    min_window_size = 15
+    max_window_size = 45
     for customer in customer_distances:
         travel_time = customer/(max_speed * 60)
         print(travel_time)
         timeWindowMin = np.random.randint(max(prev_min-max_window_size/3,0) ,max(prev_min-max_window_size/3,0)+ max_time_window_start_variation)
-        timeWindowMax = np.random.randint(max(timeWindowMin,prev_min+ travel_time) + min_window_size,timeWindowMin + max_window_size)
+        timeWindowMax = np.random.randint(max(timeWindowMin + min_window_size,prev_min+ travel_time) ,max(timeWindowMin + max_window_size,prev_min+ travel_time) )
 
         prev_min = max(timeWindowMin,prev_min+ travel_time)
         customer_timewindows.append((timeWindowMin,timeWindowMax))
@@ -54,33 +55,70 @@ def generateCustomers(num_customers, max_speed):
 
 
 max_speed = maxSpeedPowerSetting[len(maxSpeedPowerSetting)-1]
-num_customers = 30
+num_customers = 25
 #Dinstances in meters
 
 customer_distances,customer_timewindows = generateCustomers(num_customers,max_speed)
+# print(customer_distances)
+# print(customer_timewindows)
+#customer_distances = [19797, 11849, 24479, 17410, 23484, 16410 ,22480, 27691, 12591,  5551]
+#customer_timewindows = [(15, 92), (9, 180), (17, 183), (77, 237), (138, 308), (192, 345), (226, 376), (278, 432), (338, 456), (368, 542)]
 #customer_distances = [ 419,1356 ,1574 ,1751 , 692,  965 ,1155 , 364 ,1644 ,1975 ,2318, 1281, 1735, 2899, 2905, 2251, 1429, 2053,  896, 1959]
 #customer_timewindows = [(28, 73), (11, 94), (13, 69), (29, 95), (3, 133), (0, 78), (25, 82), (26, 179), (18, 193), (10, 169), (26, 96), (29, 110), (15, 118), (14, 118), (37, 164), (35, 183), (30, 164), (40, 190), (47, 223), (64, 181)]
 
 handling_time = 0
-
+timesBranched = 0
+timesDiscardedForWorse = 0
+averageDepthDiscardedWorse = 0
+timesDiscardedForNotPossible =0
+averageDepthDiscardedNotPossible = 0
+timesEndedBranching = 0
+timesEndedBranchingImprovement = 0
+timesEndedExec = 0
+start_time = time.time()
 
 
 def branchAndBound(customer_distances,customer_timewindows,speed_options,power_options,best_solution,best_solution_value,current_time,current_value,current_solution):
     max_speed = speed_options[len(speed_options)-1]
     total_dist = 0
-
+    global timesBranched
+    global timesDiscardedForNotPossible
+    global timesDiscardedForWorse
+    global timesEndedBranching
+    global timesEndedBranchingImprovement
+    global timesEndedExec
+    global averageDepthDiscardedNotPossible
+    global averageDepthDiscardedWorse
+    global handling_time
     if(len(customer_timewindows) == 0):
+        
         if (current_value<best_solution_value):
+            timesEndedBranchingImprovement += 1
             return current_solution,current_value
+        else:
+            timesEndedBranching += 1
+            return best_solution,best_solution_value
 
     #Check if there still is a solution possible
     for i in range(len(customer_distances)):
         total_dist +=customer_distances[i]
         if(total_dist/(max_speed*60) + current_time > customer_timewindows[i][1]):
+            #print("stopped because not viable on depth",len(current_solution))
+            averageDepthDiscardedNotPossible = (averageDepthDiscardedNotPossible * timesDiscardedForNotPossible + len(current_solution))/(timesDiscardedForNotPossible + 1)
+            timesDiscardedForNotPossible += 1
             return best_solution,best_solution_value
     #Check if the solution can be better than the current best
-    min_remaining_cost = total_dist/speed_options[0] * power_options[0]
+    minimum_average_speed = 0
+    for speed in range(len(speed_options)):
+        if(total_dist/(speed_options[speed]*60) + current_time <= customer_timewindows[len(customer_timewindows)-1][1]):
+            minimum_average_speed=speed
+            break
+    min_remaining_cost = total_dist/(speed_options[minimum_average_speed] * 60) * power_options[minimum_average_speed]
     if(current_value + min_remaining_cost >= best_solution_value):
+        #print("stopped because not better on depth",len(current_solution))
+        averageDepthDiscardedWorse = (averageDepthDiscardedWorse * timesDiscardedForWorse + len(current_solution))/(timesDiscardedForWorse + 1)
+
+        timesDiscardedForWorse += 1
         return best_solution,best_solution_value
     
     
@@ -96,13 +134,18 @@ def branchAndBound(customer_distances,customer_timewindows,speed_options,power_o
     #     best_solution, best_solution_value = branchAndBound(customer_distances[1:],customer_timewindows[1:],speed_options,power_options,best_solution,best_solution_value,max( current_time + customer_distances[0]/speed_options[2]*60,customer_timewindows[0][0]),current_value + customer_distances[0]/speed_options[2]*60 * power_options[2],current_solution.append(2))
 
     for i in range(len(speed_options)):
-        if(current_time + customer_distances[0]/(speed_options[i]*60) < customer_timewindows[0][1]):
+        if(current_time + customer_distances[0]/(speed_options[i]*60) < customer_timewindows[0][1]):           
+            timesBranched += 1
             best_solution, best_solution_value = branchAndBound(customer_distances[1:],customer_timewindows[1:],speed_options,power_options,best_solution,best_solution_value, max( current_time + customer_distances[0]/(speed_options[i]*60),customer_timewindows[0][0]),current_value + customer_distances[0]/(speed_options[i]*60) * power_options[i],current_solution + str(i))
-
+    timesEndedExec += 1
     return best_solution,best_solution_value
 
 print (branchAndBound(customer_distances,customer_timewindows,maxSpeedPowerSetting,power_settings,[],Infinity,0,0,""))
-
+print(timesBranched,"Time taken: ", time.time()-start_time)
+print(timesDiscardedForWorse,timesDiscardedForNotPossible,timesEndedBranching,timesEndedBranchingImprovement,timesEndedExec, timesDiscardedForWorse +timesDiscardedForNotPossible+timesEndedBranching+timesEndedBranchingImprovement+timesEndedExec)
+print(averageDepthDiscardedWorse,averageDepthDiscardedNotPossible)
+print("Total distance",sum(customer_distances))
+print(customer_timewindows)
 # print(customer_timewindows)
 # print([customer/(max_speed * 60) for customer in customer_distances])
 # print(customer_distances)
