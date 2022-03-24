e0e28223e473932d6d1bdd8b9437af86e3ba0061
samhesselmans <samcasperhesselmans@gmail.com>
2022-03-24 09:47:51 +0100

    More benchmarks

--- SA-ILP/Benchmarker/Program.cs
+++ SA-ILP/Benchmarker/Program.cs
@@ -5,11 +5,25 @@ using System.Diagnostics;
 var gamma = new Gamma(2.0, 1.5);
 var random = new Random();
 var toBenchGamma = () => { gamma.CumulativeDistribution(0.7); };
+var toBenchGammaCreation = () => { var gamma = new Gamma(2.0, 1.5); gamma.CumulativeDistribution(0.7); };
 var toBenchGammaPDF = () => { gamma.Density(0.7); };
 var toBenchRandomNumber = () =>{ random.Next(100); };
+
+var list = new List<Gamma>(100 * 100 * 150);
+
+for(int i=0; i< 100 * 100 * 150; i++)
+{
+    list.Add(new Gamma(2.0, 1.5));
+}
+
 Benchmark(toBenchGamma,"Gamma cummulative distribution");
 Benchmark(toBenchGammaPDF, "Gamma PDF ");
 Benchmark(toBenchRandomNumber, "Random number bench");
+Benchmark(toBenchGammaCreation, "Gamma creation");
+
+
+
+
 void Benchmark(Action toBenchmark,string label, int numTries = 10000000)
 {
     Console.WriteLine($"Starting benchmarking {label}");
